05a5815451ae4a15671a03ce8a1adbba29d3a9a1
jussipekka <jerkkola@gmail.com>
2018-03-04 21:38:50 +0200

    add update script

---
+++ playground/mongodb-update.js
@@ -0,0 +1,23 @@
+//const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+var obj = new ObjectID();
+
+console.log(obj);
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if (err) {
+    return console.log('Unable to connect to MongoDB server');
+  }
+  console.log('Connected to MongoDB server');
+
+  db.collection('Users').findOneAndUpdate({
+    _id: new ObjectID('5a9bcc5ba465dd64227427b5')}
+    , {  $set: {  name: 'JP' }, $inc: {age: +1}}
+    , {returnOriginal: false})
+    .then((result) => {
+    console.log(result);
+  });
+
+  //db.close();
+});
