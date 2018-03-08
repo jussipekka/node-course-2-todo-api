461c6cca62a52e8c583f1c19cae23abdaaea59ae
jussipekka <jerkkola@gmail.com>
2018-03-08 22:52:47 +0200

    Add GET /todos:id

--- server/server.js
+++ server/server.js
@@ -1,3 +1,5 @@
+var {ObjectID} = require('mongodb');
+
 var express = require('express');
 var bodyParser = require('body-parser');
 
@@ -30,6 +32,22 @@ app.get('/todos', (req, res, next) => {
   });
 
 
+});
+
+app.get('/todos/:id', (req, res, next) => {
+  var id = req.params.id;
+  if (!ObjectID.isValid(id)) {
+     return res.status(404).send();
+   }
+
+  Todo.findById(id).then((todo) => {
+     if (!todo) {
+       return res.status(404).send();
+     }
+     res.send({todo});
+  }).catch((e) => {
+    res.status(400).send();
+  });
 });
 
 
